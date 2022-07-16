cb0c6ebd12ffc08fce59f5e9b2cc1faafae35bb7
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-07-16 08:38:19 -0300

    feat: first commit

---
+++ Play Music Spoty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Play Music Spoty
+//
+//  Created by kenjimaeda on 16/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
